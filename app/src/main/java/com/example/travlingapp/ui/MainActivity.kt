560a6783a5aebc59ee4303bbf6b773e87c53e7fc
Jianglong Yu <yujianglan98@gmail.com>
2023-03-20 16:28:17 -0700

    background image

--- app/src/main/java/com/example/travlingapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/travlingapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.travlingapp
+package com.example.travlingapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.travlingapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
