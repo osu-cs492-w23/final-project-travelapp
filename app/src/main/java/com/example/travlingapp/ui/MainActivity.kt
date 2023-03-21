ff659f2e3e1980937c57c2ab7402ba076172c852
Jianglong Yu <yujianglan98@gmail.com>
2023-03-20 19:39:38 -0700

    location button created

--- app/src/main/java/com/example/travlingapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/travlingapp/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.travlingapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.travlingapp.R
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inputBoxFrom: EditText = findViewById(R.id.input_box_from)
+        val inputBoxTo: EditText = findViewById(R.id.input_box_to)
+
+        val searchBtn: Button = findViewById(R.id.btn_search)
+
+
+        searchBtn.setOnClickListener {
+            val originCity = inputBoxFrom.text.toString()
+            val destinationCity = inputBoxTo.text.toString()
+            Log.d(TAG, "origin city: $originCity, destination city: $destinationCity")
+        }
+
     }
 }
